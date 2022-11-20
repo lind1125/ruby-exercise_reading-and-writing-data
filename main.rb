93e3365ac6dd351a20c624ac6b19f39821880ab4
lind1125 <danieljlinden@gmail.com>
2022-11-19 21:41:09 -0600

    Learning read and write methods

--- main.rb
+++ main.rb
@@ -2,4 +2,28 @@ require 'csv'
 require 'json'
 require 'net/http'
 
-puts "Hello, Ruby!"
+file_location = "sample.txt"
+
+# ---Accessing files in the directory---
+# using the File.open method
+# file = File.open(file_location)
+# content = file.read
+
+#using the File.read method
+# content = File.read(file_location)
+
+# puts content
+
+# --- Writing to files---
+new_content = "New Content!"
+
+# Overwrites existing content
+# File.write(file_location, new_content)
+
+# Using additional argument to append to existing content
+# File.write(file_location, new_content, mode: "a")
+
+# Using code blocks to append to existing content
+File.open(file_location, "a") do |file|
+  file.write(new_content)
+end
